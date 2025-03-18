8d62be1b71c966df9de6e59f73f3ddda569016ea
Eric Afes <eriicafes@gmail.com>
2025-03-18 15:20:05 +0100

    Add typescript solution

---
+++ src/main.ts
@@ -0,0 +1,89 @@
+type Subscriber<T> = (data: T) => void | Promise<void>;
+
+class Event<T> {
+  private subscribers = new Set<Subscriber<T>>();
+
+  constructor(private eventBus: EventBus, public priority: "low" | "high") {}
+
+  public subscribe(fn: Subscriber<T>) {
+    this.subscribers.add(fn);
+  }
+
+  public async emit(data: T) {
+    // run high priority immediately
+    if (this.priority == "high") {
+      return await this.run(this.subscribers, data);
+    }
+    // run low priority in batches of 20
+    let batch = new Set<Subscriber<T>>();
+    for (const subscriber of this.subscribers) {
+      if (batch.add(subscriber).size >= 20) {
+        await this.runMicrotask(() => this.run(batch, data));
+        batch = new Set<Subscriber<T>>(); // reset batch
+      }
+    }
+    // run remaining batch if any
+    if (batch.size) return await this.runMicrotask(() => this.run(batch, data));
+  }
+
+  private runMicrotask(fn: () => Promise<void>) {
+    const { promise, resolve } = Promise.withResolvers();
+    queueMicrotask(() => fn().then(resolve));
+    return promise;
+  }
+
+  private run(subscribers: Set<Subscriber<T>>, data: T) {
+    return this.eventBus.run(async () => {
+      for (const subscriber of subscribers) await subscriber(data);
+    });
+  }
+}
+
+class EventBus {
+  private ready = Promise.resolve();
+
+  public async run(fn: () => Promise<void>) {
+    const ready = this.ready;
+    this.ready = new Promise(async (res) => ready.then(fn).then(res));
+    return this.ready;
+  }
+
+  public createEvent<T>(priority: "low" | "high"): Event<T> {
+    return new Event(this, priority);
+  }
+}
+
+const delay = (seconds: number) => {
+  return new Promise<void>((res) => setTimeout(res, seconds * 1000));
+};
+
+export async function main() {
+  const eventBus = new EventBus();
+  const event1 = eventBus.createEvent<string>("high");
+  const event2 = eventBus.createEvent<string>("low");
+
+  const start = Date.now();
+  const logger = (event: string, subscriber: string) => {
+    const diff = Math.round((Date.now() - start) / 1000);
+    console.log(`${diff}s:` + "\t", event + "\t", subscriber);
+    return delay(1); // 1 second delay
+  };
+
+  // add event1 subscribers
+  for (let i = 1; i <= 10; i++) {
+    event1.subscribe((data) => logger(data, `subscriber${i}`));
+  }
+  // add event2 subscribers
+  for (let i = 1; i <= 100; i++) {
+    event2.subscribe((data) => logger(data, `subscriber${i}`));
+  }
+
+  event2.emit("event2");
+  await delay(10); // 10 seconds later
+  event1.emit("event1 (t=10)");
+  await delay(10); // 20 seconds later
+  event1.emit("event1 (t=30)");
+  await delay(30); // 30 seconds later
+  event1.emit("event1 (t=50)");
+}
+main();
